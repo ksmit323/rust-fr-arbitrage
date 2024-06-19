227cabd0efd03277c75a7ed3df1361a0ba4a64f2
Kenneth Smith <ksmit323@gmail.com>
2024-06-19 10:58:32 +0700

    add files

---
+++ src/api/hyperliquid.rs
@@ -0,0 +1,79 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::error::Error;
+
+const URL_BASE: &str = " https://api.hyperliquid.xyz/info";
+
+#[derive(Serialize)]
+struct RequestBody<'a> {
+    r#type: &'a str,
+}
+
+#[derive(Deserialize, Debug)]
+struct AssetInfo {
+    name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct AssetContext {
+    funding: String,
+}
+
+pub struct Hyperliquid {
+    url: String,
+}
+
+impl Hyperliquid {
+    pub fn new() -> Self {
+        Self {
+            url: URL_BASE.to_string(),
+        }
+    }
+
+    async fn get_market_data(&self) -> Result<Value, Box<dyn Error>> {
+        // Headers for the request
+        // let headers = [("Content-Type", "application/json")];
+
+        // Request body data
+        let body = RequestBody {
+            r#type: "metaAndAssetCtxs",
+        };
+        // Send request to API
+        let client = Client::new();
+        let response = client
+            .post(&self.url)
+            // .headers(headers.into())
+            .json(&body)
+            .send()
+            .await?;
+
+        // Parse response JSON
+        let json: Value = response.json().await?;
+
+        Ok(json)
+    }
+
+    pub async fn get_funding_rates(&self) -> Result<HashMap<String, f64>, Box<dyn Error>> {
+        // Get market data
+        let market_data = self.get_market_data().await?;
+
+        // Parse asset info and context
+        let asset_info: Vec<AssetInfo> =
+            serde_json::from_value(market_data[0]["universe"].clone())?;
+        let asset_context: Vec<AssetContext> = serde_json::from_value(market_data[1].clone())?;
+
+        // Initialize Hashmap
+        let mut funding_rates: HashMap<String, f64> = HashMap::new();
+
+        // Iterate over both lists, assuming their indexes are aligned
+        for (asset, context) in asset_info.iter().zip(asset_context.iter()) {
+            let symbol = asset.name.clone();
+            let funding_rate: f64 = context.funding.parse::<f64>()? * 100.0; // Convert 1hr rate to percentage
+            funding_rates.insert(symbol, funding_rate);
+        }
+
+        Ok(funding_rates)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,125 @@
-fn main() {
-    println!("Hello, world!");
+// mod api;
+
+// #[tokio::main]
+// async fn main() {
+//     // main::main_class::run()
+
+//     let response = api::hyperliquid::Hyperliquid::new()
+//         .get_funding_rates()
+//         .await;
+//     println!("response = {:?}", response.unwrap());
+// }
+
+use ethers::contract::{Contract, ContractFactory};
+use ethers::prelude::*;
+use ethers::providers::{Http, Provider};
+use ethers::types;
+use ethers::types::{Address, U256};
+use ethers::utils::Anvil;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+use tokio;
+
+#[derive(Deserialize)]
+struct Base {
+    chain_id: u64,
+    rpc_url: String,
 }
+
+#[derive(Deserialize)]
+struct MarketMetaData {
+    market_name: String,
+    symbol: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Generate the contract from the abi
+    abigen!(PerpsMarketProxy, "src/abi/PerpsMarketProxy.json",);
+
+    let symbols: [(u128, &str); 32] = [
+        (200, "BTC"),
+        (100, "ETH"),
+        (300, "SNX"),
+        (400, "SOL"),
+        (600, "W"),
+        (500, "WIF"),
+        (1600, "ARB"),
+        (900, "AVAX"),
+        (1800, "BNB"),
+        (1400, "BONK"),
+        (800, "DOGE"),
+        (700, "ENA"),
+        (1500, "FTM"),
+        (1700, "MATIC"),
+        (1000, "OP"),
+        (1100, "ORDI"),
+        (1200, "PEPE"),
+        (1300, "RUNE"),
+        (2600, "ARKM"),
+        (3200, "AXL"),
+        (2900, "BOME"),
+        (3000, "ETHFI"),
+        (2700, "GALA"),
+        (2200, "GMX"),
+        (2100, "INJ"),
+        (1900, "LINK"),
+        (2000, "PENDLE"),
+        (3100, "STX"),
+        (2400, "SUI"),
+        (2800, "TAO"),
+        (2300, "TIA"),
+        (2500, "TON"),
+    ];
+
+    let base = Base {
+        chain_id: 8453,
+        rpc_url: "https://mainnet.base.org".to_string(),
+    };
+
+    let provider = Provider::<Http>::try_from(base.rpc_url)?;
+
+    let perps_market_proxy_address: Address =
+        "0x0A2AF931eFFd34b81ebcc57E3d3c9B1E1dE1C9Ce".parse()?;
+
+    // let perps_market_proxy_abi = serde_json::from_str(include_str!("PerpsMarketProxy.json"))?;
+
+    // Create a contract instance
+    // let contract = Contract::new(perps_market_proxy_address, perps_market_proxy_abi, provider.clone());
+
+    let contract = PerpsMarketProxy::new(perps_market_proxy_address, provider.clone().into());
+
+    // Call the getMarkets function to get market IDs
+    // let market_ids = contract.get_markets().call().await?;
+
+    let mut funding_rates: HashMap<String, f64> = HashMap::new();
+
+    for symbol in symbols {
+        let market = contract.get_market_summary(symbol.0).call().await?;
+        // Convert funding rate from wei to eth and multiply by 100 to convert to percentage and divide by 24 to get hourly
+        let fr = convert_to_hourly_percent(wei_to_eth(market.current_funding_rate).unwrap());
+        funding_rates.insert(symbol.1.to_string(), fr);
+    }
+
+    println!("FR = {:?}", funding_rates);
+
+    Ok(())
+}
+
+fn wei_to_eth(value: I256) -> Result<f64, Box<dyn Error>> {
+    let wei_value = value.to_string().parse::<f64>()?;
+    let decimals = 18;
+    let conversion_factor = 10u64.pow(decimals).to_string().parse::<f64>()?;
+    Ok(wei_value / conversion_factor)
+}
+
+fn convert_to_hourly_percent(value: f64) -> f64 {
+    value * 100.0 / 24.0
+}
+
+//TODO:
+// Convert market IDS to some tickers
+// Create a mapping from tickers to funding rates
+// Convert funding rates to proper percentages
